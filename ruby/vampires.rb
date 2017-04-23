ef26a0aa25c63f86b7b3d0c1c76a7b959484c839
Anoushka Vaswani <vaswani.anoushka@gmail.com>
2017-04-23 16:15:07 -0700

    4.4

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -4,69 +4,75 @@ i = 1
 
 while i <= employees
 
-puts "What is your name?"
-name=gets.chomp
+	puts "What is your name?"
+	name=gets.chomp
 
-if name=="Drake Cula"
-	name_check=false
-elsif name=="Tu Fang"
-	name_check=false
-else
-	name_check==true
-end
+	if name=="Drake Cula"
+		name_check=false
+	elsif name=="Tu Fang"
+		name_check=false
+	else
+		name_check==true
+	end
 
-puts "name any allergies. type done when finished"
+	puts "How old are you?"
+	age=gets.chomp.to_i
 
-while allergies = gets.chomp
-    break if (allergies == "done")
-end
+	puts "What year were you born?"
+	year=gets.chomp.to_i
 
-puts "How old are you?"
-age=gets.chomp.to_i
+	age_test=2017-year
 
-puts "What year were you born?"
-year=gets.chomp.to_i
+	if age_test==age
+		age_check=true
+	else
+		age_check=false
+	end
 
-age_test=2017-year
+	puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+	bread=gets.chomp
 
-if age_test==age
-	age_check=true
-else
-	age_check=false
-end
-
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
-bread=gets.chomp
-
-if bread=="y"
-	bread_check=true
-else
-	bread_check=false
-end
+	if bread=="y"
+		bread_check=true
+	else
+		bread_check=false
+	end
 
 
-puts "Would you like to enroll in the company’s health insurance? (y/n)"
-insurance=gets.chomp
+	puts "Would you like to enroll in the company’s health insurance? (y/n)"
+	insurance=gets.chomp
 
-if insurance=="y"
-	insurance_check=true
-else
-	insurance_check=false
-end
+	if insurance=="y"
+		insurance_check=true
+	else
+		insurance_check=false
+	end
 
-if name_check==false
-	result = "Definitely a vampire."
-elsif !age_check&&!bread_check&&!insurance_check==true
-	result = "Almost certainly a vampire."
-elsif !age_check && (bread_check||insurance_check)==true
-	result = "Probably a vampire"
-elsif age_check && (bread_check||insurance_check)==true
-	result = "Probably not a vampire"
-else
-	result = "Results inconclusive"
-end
+	if name_check==false
+		result = "Definitely a vampire."
+	elsif !age_check&&!bread_check&&!insurance_check==true
+		result = "Almost certainly a vampire."
+	elsif !age_check && (bread_check||insurance_check)==true
+		result = "Probably a vampire"
+	elsif age_check && (bread_check||insurance_check)==true
+		result = "Probably not a vampire"
+	else
+		result = "Results inconclusive"
+	end
 
-puts result 
+	puts result 
+
+	puts "name any allergies. type done when finished"
+		employee_input = ""
+	until employee_input == "done"
+		employee_input = gets.chomp
+		if employee_input == "sunshine"
+			puts "Probably a vampire."
+      		employee_input = "done"
+   		else 
+     	 	puts "Add allergies or type 'done' to finish:"
+ 	 	end
+ 	 end
 
 i+= 1
 
